2005f05416ef88948e75e0ffe39aca795cf544f1
Virupakshi <virupakshivijji@gmail.com>
2025-02-20 23:29:31 +0530

    added router for submit feedback button

--- src/app/components/EmployeeDashboard.tsx
+++ src/app/components/EmployeeDashboard.tsx
@@ -89,7 +89,10 @@ export default function EmployeeDashboard() {
             </p>
           </div>
           <div className="mt-6 flex gap-4">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md w-1/2">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md w-1/2"
+              onClick={() => router.push("/feedback")}
+            >
               + Submit Feedback
             </button>
             <button className="bg-green-500 text-white px-4 py-2 rounded-md w-1/2">
